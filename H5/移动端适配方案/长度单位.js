1d60041bdbced3da6b4e6d376693a37b799b978d
Ellasun1129 <sunlingzhi1129@163.com>
2023-04-18 11:50:12 +0800

    Update 长度单位.js

--- H5/移动端适配方案/长度单位.js
+++ H5/移动端适配方案/长度单位.js
@@ -1,5 +1,5 @@
 绝对长度
-em、ex、ch、rem、vmin、vmax、%
+em、ex、ch、rem、%
 
 相对长度
 cm、mm、in、px、pt、pc
@@ -7,3 +7,5 @@ cm、mm、in、px、pt、pc
 视口比例的长度
 1. vw：viewpoint width，视窗宽度，1vw等于视窗宽度的1%。
 2. vh：viewpoint height，视窗高度，1vh等于视窗高度的1%。
+3. vmin：取当前vw和vh中较小的那一个值。
+4. vmax：取当前vw和vh中较大的那一个值。
